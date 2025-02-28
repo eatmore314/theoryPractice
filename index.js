250fd1a98bea14926194472e821a5fd69364c20b
Walter White <walterwhite@DPEMS-MBA920.lan>
2025-02-27 19:38:51 -0500

    keystroke notes 1

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-//Iterating through objects
+let inputText = document.querySelector(".input_text");
+
+inputText.addEventListener("keydown", ()=>{
+    console.log("You pressed A KEY!")
+})
+
+//Adding keystroke to ENTIRE document.
+document.addEventListener("keydown", ()=>{
+    console.log("No matter what key you press im logged!!")
+});
+
+//Logs which key you pressed aswell
+document.addEventListener("keydown", function(event) {
+    console.log("Key down:", event.key);
+  });
+
+  //Keyup: Fires when a key is "released" (only once)
+  
